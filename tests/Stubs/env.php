86fac32c7abeaaf6e16808b1e7e6a1c4997815db
Devaing Group <git@devaing.net>
2024-09-07 19:59:11 +0100

    Update env.php

--- tests/Stubs/env.php
+++ tests/Stubs/env.php
@@ -7,8 +7,8 @@
  */
 return [
     "prefix" => "rave",
-    "secretKey" => "",
-    "publicKey" => "",
+    "secretKey" => "FLWSECK-4127f15e63c9098402dcc7891798fb0f-X",
+    "publicKey" => "FLWPUBK-1cf610974690c2560cb4c36f4921244a-X",
     "title" => "Rave Payment Gateway",
     "env" => "testing",
     "logo" => "https://files.readme.io/ee907a0-small-rave_by_flutterwave.png"
